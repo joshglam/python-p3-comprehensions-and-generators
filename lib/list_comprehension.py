b2192582f1ceea2db5b78f6445df541e7767910d
josh <jglam88@gmail.com>
2023-10-11 15:10:07 -0400

    completes lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+l = [1, 2, 3, 4, 5]
+
+
 
 def return_evens(num_list):
-    pass
+    # evens = []
+    # for n in num_list:
+    #     if n % 2 == 0:
+    #         evens.append(n)
+    evens = [n for n in num_list if n % 2 == 0 ]
+    return evens
+
+    
 
 def make_exclamation(sentence_list):
-    pass
+    exclaim = [sent + "!" for sent in sentence_list if len(sent) > 1]
+    return exclaim
+
+k = ["Hello", "I'm doing great", "Python is fun"]
